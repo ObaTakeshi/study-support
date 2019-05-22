b0ffaab4f324becfa68404bb84b43fcb2cec29e2
ObaTakeshi <s153025@matsu.shimane-u.ac.jp>
2019-05-23 02:48:00 +0900

    taskTreeのレイアウト変更

--- src/renderer/components/taskList.vue
+++ src/renderer/components/taskList.vue
@@ -3,7 +3,7 @@
     <!-- タスクリスト -->
     <h1 class="tasklisth1">Task List</h1>
     <el-button class="ml-2" size="mini" type="info" @click="top_append">Top Append</el-button>
-    <div class="custom-tree-container">
+    <div class="custom-tree-container mt-2">
       <div class="block">
         <el-tree
           :data="data"
@@ -38,7 +38,7 @@
       var _this = this;
       this.taskListRef.on('value', function(snapshot) {
         if (snapshot.val() == null) {
-          _this.data = [{id:1, label:'New Task', status:'wip'}]
+          _this.data = [{id:1, label:'New Task', status:'wip', level: 1}]
           _this.taskListRef.set(JSON.parse(JSON.stringify(_this.data))); // JSON送信
         } else {
           _this.data = snapshot.val(); // データに変化が起きたときに再取得する
@@ -58,14 +58,17 @@
           showCancelButton: true,
           cancelButtonText: 'Cancel',
         }).then(({ value }) => {
-          let append_form = document.forms.append_task;
-          const newChild = { id: new_id, label: append_form.task_name.value, status: 'wip', children: [] };
+          let append_task = document.forms.append_task.task_name.value;
+          if (append_task == '') {
+            append_task = placeholder;
+          }
+          const newChild = { id: new_id, label: append_task, status: 'wip', level: 1, children: [] };
           let new_key =  Math.max(...Object.keys(this.data)) + 1;
           this.data[new_key] = newChild;
           this.taskListRef.set(JSON.parse(JSON.stringify(this.data))); // JSON送信
           this.$message({
             type: 'success',
-            message: 'New task is:' + append_form.task_name.value
+            message: 'New task is:' + append_task
           });
         }).catch(() => {
           this.$message({
@@ -86,8 +89,12 @@
           showCancelButton: true,
           cancelButtonText: 'Cancel',
         }).then(({ value }) => {
-          let append_form = document.forms.append_task;
-          const newChild = { id: new_id, label: append_form.task_name.value, status: 'wip', children: [] };
+          let append_task = document.forms.append_task.task_name.value;
+          if (append_task == '') {
+            append_task = placeholder;
+          }
+          let new_level = data.level + 1;
+          const newChild = { id: new_id, label: append_task, status: 'wip', level: new_level, children: [] };
           if (!data.children) {
             this.$set(data, 'children', []);
           }
@@ -95,7 +102,7 @@
           this.taskListRef.set(JSON.parse(JSON.stringify(this.data))); // JSON送信
           this.$message({
             type: 'success',
-            message: 'New task is:' + edit_form.task_name.value
+            message: 'New task is:' + append_task
           });
         }).catch(() => {
           this.$message({
@@ -121,12 +128,14 @@
           showCancelButton: true,
           cancelButtonText: 'Cancel',
         }).then(({ value }) => {
-          let edit_form = document.forms.edit_task;
-          data.label = edit_form.task_name.value;
+          let edit_task = document.forms.edit_task.task_name.value;
+          if (!(edit_task == '')) {
+            data.label = edit_task;
+          }
           this.taskListRef.set(JSON.parse(JSON.stringify(this.data))); // JSON送信
           this.$message({
             type: 'success',
-            message: 'New task is:' + edit_form.task_name.value
+            message: 'New task is:' + edit_task
           });
         }).catch(() => {
           this.$message({
@@ -137,15 +146,37 @@
       },
 
       renderContent(h, { node, data, store }) {
-        return (<span class="custom-tree-node">
-            <span>{node.label}</span>
-            <span>
-              <el-button size="mini" type="success" on-click={ () => this.edit(data) }>Edit</el-button>
-              <el-button size="mini" type="text" on-click={ () => this.append(data) }>Append</el-button>
-              <el-button size="mini" type="danger" on-click={ () => this.remove(node, data) }>Delete</el-button>
-            </span>
-          </span>
-        );
+        if (data.level == 1) {
+          return (<span class="custom-tree-node">
+              <span><h3>{node.label}</h3></span>
+                <span>
+                  <el-button size="mini" type="success" on-click={ () => this.edit(data) }>Edit</el-button>
+                  <el-button size="mini" type="text" on-click={ () => this.append(data) }>Append</el-button>
+                  <el-button size="mini" type="danger" on-click={ () => this.remove(node, data) }>Delete</el-button>
+                </span>
+              </span>
+          );
+        } else if (data.level == 2) {
+          return (<span class="custom-tree-node">
+              <span><h4>{node.label}</h4></span>
+                <span>
+                  <el-button size="mini" type="success" on-click={ () => this.edit(data) }>Edit</el-button>
+                  <el-button size="mini" type="text" on-click={ () => this.append(data) }>Append</el-button>
+                  <el-button size="mini" type="danger" on-click={ () => this.remove(node, data) }>Delete</el-button>
+                </span>
+              </span>
+          );
+        } else {
+          return (<span class="custom-tree-node">
+              <span><h5>{node.label}</h5></span>
+                <span>
+                  <el-button size="mini" type="success" on-click={ () => this.edit(data) }>Edit</el-button>
+                  <el-button size="mini" type="text" on-click={ () => this.append(data) }>Append</el-button>
+                  <el-button size="mini" type="danger" on-click={ () => this.remove(node, data) }>Delete</el-button>
+                </span>
+              </span>
+          );
+        }
       }
     }
   }
