e2674ab3cb16c74ec829a2609b09ae7e48ce972c
ObaTakeshi <s153025@matsu.shimane-u.ac.jp>
2019-06-24 00:41:11 +0900

    markdownの内容をcloudと共有

--- src/renderer/components/markdownEditor.vue
+++ src/renderer/components/markdownEditor.vue
@@ -1,10 +1,12 @@
 <template>
   <div id="main">
+    <el-button size="mini" type="info" @click="save">save</el-button>
     <mavon-editor v-model="value" language="en" :toolbars="toolbars" />
   </div>
 </template>
 
 <script>
+  import firebase from 'firebase'
   import mavonEditor from 'mavon-editor'
   require('mavon-editor/dist/css/index.css')
   export default {
@@ -42,12 +44,34 @@
           trash: false,
           save: false,
           navigation: false,
-        }
+        },
+        markdownRef: null,
+        database: null,
       };
     },
 
     components: {
       'mavon-editor': mavonEditor.mavonEditor
+    },
+
+    created: function() {
+      this.database = firebase.database();
+      this.markdownRef = this.database.ref('markdown');
+      var _this = this;
+      this.markdownRef.on('value', function(snapshot) {
+        if (snapshot.val() == null) {
+          _this.value = ""
+        } else {
+          _this.value = snapshot.val(); // データに変化が起きたときに再取得する
+        }
+      });
+    },
+
+    methods: {
+      save: function() {
+        console.log(this.value);
+        this.markdownRef.set(this.value);
+      }
     }
   }
 </script>
