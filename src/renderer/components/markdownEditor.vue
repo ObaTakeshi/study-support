afe0b625fda9ad6eda509ac5e3a4e21fcc43f31c
ObaTakeshi <t1233banana@yahoo.co.jp>
2020-03-25 18:56:00 +0900

    Markdown: 変更があった場合定期実行で保存

--- src/renderer/components/markdownEditor.vue
+++ src/renderer/components/markdownEditor.vue
@@ -1,6 +1,13 @@
 <template>
   <div id="main">
-    <el-button size="mini" type="primary" @click="save">save</el-button>
+    <div class="row">
+      <div class="col-2">
+        <el-button size="mini" type="primary" @click="save">save</el-button>
+      </div>
+      <div class="offset-5 col-5">
+        <p class="last-saved-text">saved: {{ last_saved_date }}</p>
+      </div>
+    </div>
     <!-- <el-button size="mini" type="primary" @click="createPDF">print to PDF</el-button> -->
     <mavon-editor v-model="value" language="en" :toolbars="toolbars" />
   </div>
@@ -8,6 +15,7 @@
 
 <script>
   import { ipcRenderer } from 'electron'
+  import moment from 'moment'
   import firebase from 'firebase'
   import mavonEditor from 'mavon-editor'
   require('mavon-editor/dist/css/index.css')
@@ -49,7 +57,10 @@
         },
         markdownRef: null,
         database: null,
-        uid: firebase.auth().currentUser.uid
+        uid: firebase.auth().currentUser.uid,
+        interval_id: null,
+        last_saved_date: "None",
+        pre_value: ''
       }
     },
 
@@ -73,6 +84,11 @@
           _this.value = snapshot.val() // データに変化が起きたときに再取得する
         }
       });
+      this.interval_id = setInterval(function() {
+        if (this.value !== this.pre_value) {
+          this.save()
+        }
+      }.bind(this), 5000);
     },
 
     methods: {
@@ -80,11 +96,13 @@
         // console.log(this.value)
         this.markdownRef.set(this.value, function(error) {
           if (error) {
-            console.log(error)
+            alert(error)
           } else {
             console.log('saved')
           }
         });
+        this.pre_value = this.value
+        this.last_saved_date = moment().format("YYYY-MM-DD-HH:mm:ss")
       },
 
       createPDF: function() {
@@ -116,6 +134,7 @@
 
     beforeDestroy: function() {
       this.save()
+      clearInterval(this.interval_id)
     }
   }
 </script>
